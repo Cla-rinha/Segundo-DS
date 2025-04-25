383abf6cd927e88f27af4721f3b90d1e832ba41b
Maria Alves <mariaclaraalvesoliveira53@gmail.com>
2025-04-25 14:58:00 -0300

    Add files via upload
    Aqui estão salvos todos os meus códigos do meu segundo ano de curso de Desenvolvimento de Sistemas.

---
+++ PAMI/app/Etim PamI App do Zero/EtimPamIAppdoZero/app/src/main/java/br/com/fabioclaret/etimpamiappdozero/LoginActivity.kt
@@ -0,0 +1,18 @@
+package br.com.fabioclaret.etimpamiappdozero
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var email:EditText
+    private lateinit var senha:EditText
+    private lateinit var entrar:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
