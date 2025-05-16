a3b97d4481f9df8b8e181a1e80b2be3da6cd3431
Maria Alves <mariaclaraalvesoliveira53@gmail.com>
2025-05-16 16:13:46 -0300

    Delete PHP directory

--- PHP/login/usuario.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE usuariopwii;
-use usuario;
-CREATE TABLE usuarios(
-    id int AUTO_INCREMENT PRIMARY KAY,
-    nome varchar(50),
-    email varchar(100),
-    senha varchar (32)
-)       
