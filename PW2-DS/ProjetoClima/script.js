b3a27b8d7f4c9310f1b879b7dae36ec4b180d878
Maria Alves <mariaclaraalvesoliveira53@gmail.com>
2025-10-10 14:37:38 -0300

    Add files via upload

--- PW2-DS/ProjetoClima/script.js
+++ PW2-DS/ProjetoClima/script.js
@@ -1,10 +1,18 @@
-document.querySelector('.busca').addEventListener('submit', (event)=>{
+document.querySelector('.busca').addEventListener('submit', async(event)=>{
     event.preventDefault()
 
     let input = document.querySelector('#searchIput').value
 
-    if(Input !== ""){
+    if(input !== ""){
         showWarning("Carregando...")
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${ encodeURI(input) }
+        &appid=c5fcf739214f8c7a5f4e9bfa6db11199&units=metrics&lang=pt_br`
+
+        let result = await fetch(url)
+        let json   = await result.json
+
+        console.log(json)
     }else{
 
     }
