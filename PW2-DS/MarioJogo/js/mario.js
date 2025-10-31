0b4ad50eac2879de280ff6abf026a7e2b66e6bd2
Maria Alves <mariaclaraalvesoliveira53@gmail.com>
2025-10-31 15:17:25 -0300

    Add files via upload

--- PW2-DS/MarioJogo/js/mario.js
+++ PW2-DS/MarioJogo/js/mario.js
@@ -12,10 +12,24 @@ document.addEventListener('keydown', jump)
 
 const loop = setInterval(()=>{
     const pipePosition = pipe.offsetLeft
-    console.log(pipePosition)
+    const marioPosition = +getComputedStyle(mario).bottom.replace(`px`,"")
+    const clouds = document.querySelector(".clouds");
+    const cloudsPosition = clouds.offsetLeft;
 
-    if(pipePosition <= 120){
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 110){
         pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = `none`
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = './images/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clouds.style.animation = "none";
+        clouds.style.left = `${cloudsPosition}px`
+        clearInterval(loop)
     }
 
 }, 10)
